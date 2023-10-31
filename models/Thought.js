6f286355305aa4f219f43e0b5b814767f2faac24
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-10-31 15:39:28 -0400

    added Thought.js file

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: timeStamp => timeStamp
+        },
+        reactions: [
+            reactionSchema
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+const Thought = model('course', thoughtSchema);
+
+module.exports = Thought;
