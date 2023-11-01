bac24b00ba8e8adc28578b8bc2e53b4ce7706c44
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-11-01 17:10:07 -0400

    added routes to thoughtRoutes.js file

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -19,4 +19,8 @@ router
     .put(updateThought)
     .delete(deleteThought);
 
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
 module.exports = router;
