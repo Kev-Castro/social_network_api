28ebb7134c7f1b77957fc46e42fc4d63661d1846
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-11-01 17:55:27 -0400

    adding delete a reaction to thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -119,4 +119,24 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    // Delete reaction from a thought
+    async removeReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: { reactionId: req.params.friendId } } },
+                { new: true }
+            );
+
+            if (!thought) {
+                return res
+                    .status(404)
+                    .json({ message: 'No thought found with that ID :(' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 };
